ad0d810f01e20a3f23a277d344b7876757241e74
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2025-03-24 10:44:18 +0530

    firebase config updated

--- firebase/client.ts
+++ firebase/client.ts
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA2FcQWXIQTGsJT3oJI48hmpeHHkbUTLZA",
-  authDomain: "ai-interviewer-3c9bc.firebaseapp.com",
-  projectId: "ai-interviewer-3c9bc",
-  storageBucket: "ai-interviewer-3c9bc.firebasestorage.app",
-  messagingSenderId: "56469835427",
-  appId: "1:56469835427:web:acdd8d60cc58a7435af916",
-  measurementId: "G-FHB8PV277M"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
